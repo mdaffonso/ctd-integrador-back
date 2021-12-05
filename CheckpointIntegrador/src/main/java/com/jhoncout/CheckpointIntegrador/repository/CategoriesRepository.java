79e8dac63dfbfa92d78fa48e43c3ebe053b1b93d
JonathanCout <jqcoutinho.eng@gmail.com>
2021-12-05 15:15:26 -0300

    Added query to find category by its name

--- CheckpointIntegrador/src/main/java/com/jhoncout/CheckpointIntegrador/repository/CategoriesRepository.java
+++ CheckpointIntegrador/src/main/java/com/jhoncout/CheckpointIntegrador/repository/CategoriesRepository.java
@@ -2,8 +2,11 @@ package com.jhoncout.CheckpointIntegrador.repository;
 
 import com.jhoncout.CheckpointIntegrador.model.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriesRepository extends JpaRepository<Categories,Integer> {
+
+    Categories findByName(String name);
 }
