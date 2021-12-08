d2caf486e21297fc2b7398fd6b6f1097f6ee01cc
JonathanCout <jqcoutinho.eng@gmail.com>
2021-12-08 11:05:02 -0300

    added DTO to convert products price

---
+++ src/main/java/com/jhoncout/CheckpointIntegrador/dto/CategoriesDTO.java
@@ -0,0 +1,42 @@
+package com.jhoncout.CheckpointIntegrador.dto;
+
+import com.jhoncout.CheckpointIntegrador.model.Categories;
+import com.jhoncout.CheckpointIntegrador.model.Products;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoriesDTO {
+    private Integer id;
+    private String name;
+    private List<ProductsDTO> products;
+
+    public CategoriesDTO(Categories category){
+        this.id = category.getId();
+        this.name = category.getName();
+        this.products = convertListProduct(category.getProducts());
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ProductsDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductsDTO> products) {
+        this.products = products;
+    }
+
+    private List<ProductsDTO> convertListProduct(List<Products> list){
+        return list.stream().map(ProductsDTO::new).collect(Collectors.toList());
+    }
+}
